1c758f3bab55ed73352062daa247de2c5ddf7178
Dmytro <popovfreewind@gmail.com>
2025-02-23 16:30:12 +0100

    initialize project with Webpack, TypeScript, and basic HTML structure

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import './style.css';
+
+const domNode = document.getElementById('root');
+domNode.innerHTML = '';
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production';
+
+    return {
+        entry: './src/index.ts',
+        mode: argv.mode,
+        devtool: 'source-map',
+        devServer: {
+            static: path.resolve(__dirname, 'dist'),
+            port: 3000,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader'],
+                },
+                {
+                    test: /\.ts$/,
+                    loader: 'ts-loader'
+                  },
+            ],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './src/index.html',
+            }),
+        ],
+    };
+};
